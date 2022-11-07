0bcea7e4a90cc2dd2d73e8cb83fd5227cde9bf96
Roy Adonis Carrasco H <carrasco28adonis@gmail.com>
2022-11-06 23:04:48 -0500

    Se agrega obtener tipo de cambio por tipo de moneda

--- src/main/java/com/nttdata/bootcamp/msexchangerate/application/ExchangeRateService.java
+++ src/main/java/com/nttdata/bootcamp/msexchangerate/application/ExchangeRateService.java
@@ -7,8 +7,14 @@ import reactor.core.publisher.Mono;
 
 public interface ExchangeRateService {
     public Flux<ExchangeRate> findAll();
+
     public Mono<ExchangeRate> findById(String idExchangeRate);
+
     public Mono<ExchangeRate> save(ExchangeRatetDto exchangeRateDto);
+
     public Mono<ExchangeRate> update(ExchangeRatetDto exchangeRateDto, String idExchangeRate);
+
     public Mono<Void> delete(String idExchangeRate);
+
+    public Mono<ExchangeRate> findByCurrencyType(String currencyType);
 }
